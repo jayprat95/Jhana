08f6ee4026af4b60657c33f869d53a62366982cc
Jayanth Prathipati <jayanth@vt.edu>
2015-10-07 16:36:50 -0400

    Initial Commit

---
+++ Jhana/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jhana
+//
+//  Created by Jayanth Prathipati on 10/7/15.
+//  Copyright © 2015 TouchTap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
